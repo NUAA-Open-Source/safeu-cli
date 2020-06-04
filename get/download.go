5ea0f2970d3c1c86df8474d785baebf562a1b64f
TripleZ <me@triplez.cn>
2020-06-04 22:53:08 +0800

    add get CSRF & validation API for get method

--- get/download.go
+++ get/download.go
@@ -2,14 +2,38 @@ package get
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+
+	"github.com/arcosx/Nuwa/util"
 )
 
+// https://app.swaggerhub.com/apis-docs/a2os/safeu/1.0.0-rc#/miscellaneous/getCSRFToken
+func (item *ItemDownload) getCsrf() error {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", util.SAFEU_BASE_URL+"/csrf", nil)
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	item.csrf = resp.Header.Get("X-Csrf-Token")
+	item.cookie = resp.Header.Get("Set-Cookie")
+	return nil
+}
+
+// https://app.swaggerhub.com/apis-docs/a2os/safeu/1.0.0-rc#/validation/validateRC
+func (item *ItemDownload) validation() error {
+	// validate recode & password
+	return nil
+}
+
+// https://app.swaggerhub.com/apis-docs/a2os/safeu/1.0.0-rc#/download/downloadItem
 func (item *ItemDownload) download() error {
 	// download logics
 	return nil
 }
 
+// https://app.swaggerhub.com/apis-docs/a2os/safeu/1.0.0-rc#/download/downloadAwareness
 func (item *ItemDownload) minusDownCount() error {
 	// minus download counter
 	return nil
@@ -21,7 +45,19 @@ func Start(recode string, password string) {
 	item.userRecode = recode
 	item.userPassword = password
 
-	err := item.download()
+	err := item.getCsrf()
+	if err != nil {
+		fmt.Println("get CSRF token error")
+		os.Exit(0)
+	}
+
+	err = item.validation()
+	if err != nil {
+		fmt.Println("validation error")
+		os.Exit(0)
+	}
+
+	err = item.download()
 	if err != nil {
 		fmt.Println("download error")
 		os.Exit(0)
--- get/model.go
+++ get/model.go
@@ -4,4 +4,7 @@ package get
 type ItemDownload struct {
 	userRecode   string // 提取码
 	userPassword string // 密码
+	csrf         string // CSRF 口令
+	cookie       string // Cookie
+	token        string // 临时认证 Token
 }
